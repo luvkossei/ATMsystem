8b6316102de179c7f36b0269e0b2bde32e2381e2
Kousei Yoshida <kstarao0217@icloud.com>
2025-08-01 12:35:11 +0900

    記録追加

--- Withdrawal.java
+++ Withdrawal.java
@@ -1,12 +1,14 @@
 public class Withdrawal extends Transaction {
     private int amount;
     private CashDispenser dispenser;
+    private final Logger logger;
 
-    public Withdrawal(UserAccount account, int amount, CashDispenser dispenser) {
+    public Withdrawal(UserAccount account, int amount, CashDispenser dispenser,Logger logger) {
         super(account);
         if(amount <= 0) throw new IllegalArgumentException("金額は正の値でなければなりません");
         this.amount = amount;
         this.dispenser = dispenser;
+	this.logger = logger;
     }
 
     @Override
@@ -18,5 +20,13 @@ public class Withdrawal extends Transaction {
             throw new IllegalStateException("現金排出に失敗しました");
         }
         account.withdraw(amount);
+
+	TransactionRecord record = new TransactionRecord(
+            java.time.LocalDateTime.now(),
+            "WITHDRAWAL", 
+            amount,
+            account.getBalance() 
+        );
+        logger.log(record); 
     }
 }
